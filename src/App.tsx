c8d176ea8eab771089aa4074f7b312513f13ba01
nuwa37 <yond41m3@icloud.com>
2024-01-12 13:22:51 +0600

    added new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Card, {CardVariant} from './components/Card';
 
 const App = () => {
   return (
     <div className="App">
-      works
+      <Card 
+        variant={CardVariant.primary}
+        width='250px'
+        height='250px'
+      >
+        <button>
+          btn
+        </button>
+      </Card>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,39 @@
+import React, {FC} from 'react';
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary'
+}
+
+interface CardProps  {
+    width?: string;
+    height?: string;
+    variant: CardVariant;
+}
+
+const Card: FC<CardProps> = 
+    ({
+        width, 
+        height, 
+        variant,
+        children,
+    }) => {
+        return (
+            <div style = 
+                {{
+                    width, 
+                    height, 
+                    border: variant === CardVariant.outlined 
+                        ? '2px solid salmon'
+                        : 'none',
+                    background: variant === CardVariant.primary
+                        ? 'salmon'
+                        : 'none',
+                }}
+            >
+                {children}
+            </div>
+        )
+    }
+
+export default Card;
